661531bc6dd75076ca7925b835e9d34d96e304e8
sangjingyu <sjk6984@naver.com>
2026-02-24 16:27:15 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -897,7 +897,7 @@ ${ci}
 
   try {
     const res = await fetch(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=' + ak,
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=' + ak,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
